2be4e8868edfe11b50e829ed2ce9fa3cbf64c4bf
Robert Carter <racarter1215@gmail.com>
2020-07-21 15:39:53 -0700

    initial commit

--- AsynchInnLab/Migrations/20200721215433_addSeededData.Designer.cs
+++ AsynchInnLab/Migrations/20200721215433_addSeededData.Designer.cs
@@ -3,14 +3,16 @@ using AsynchInnLab.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AsynchInnLab.Migrations
 {
     [DbContext(typeof(AsynchInDbContext))]
-    partial class AsynchInDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200721215433_addSeededData")]
+    partial class addSeededData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- AsynchInnLab/Models/Amenity.cs
+++ AsynchInnLab/Models/Amenity.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace AsynchInnLab.Properties.Models
+namespace AsynchInnLab.Models
 {
     public class Amenity
     {
