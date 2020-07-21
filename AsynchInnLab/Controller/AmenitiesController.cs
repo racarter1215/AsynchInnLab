f97dbb2e940458f0906fe2247bf68a0b440298f6
Robert Carter <racarter1215@gmail.com>
2020-07-21 16:05:24 -0700

    got postman to work!

---
+++ AsynchInnLab/Controller/AmenitiesController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AsynchInnLab.Data;
+using AsynchInnLab.Models;
+
+namespace AsynchInnLab.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AmenitiesController : ControllerBase
+    {
+        private readonly AsynchInDbContext _context;
+
+        public AmenitiesController(AsynchInDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Amenities
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Amenity>>> GetAmenities()
+        {
+            return await _context.Amenities.ToListAsync();
+        }
+
+        // GET: api/Amenities/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Amenity>> GetAmenity(int id)
+        {
+            var amenity = await _context.Amenities.FindAsync(id);
+
+            if (amenity == null)
+            {
+                return NotFound();
+            }
+
+            return amenity;
+        }
+
+        // PUT: api/Amenities/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAmenity(int id, Amenity amenity)
+        {
+            if (id != amenity.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(amenity).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AmenityExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Amenities
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Amenity>> PostAmenity(Amenity amenity)
+        {
+            _context.Amenities.Add(amenity);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAmenity", new { id = amenity.Id }, amenity);
+        }
+
+        // DELETE: api/Amenities/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Amenity>> DeleteAmenity(int id)
+        {
+            var amenity = await _context.Amenities.FindAsync(id);
+            if (amenity == null)
+            {
+                return NotFound();
+            }
+
+            _context.Amenities.Remove(amenity);
+            await _context.SaveChangesAsync();
+
+            return amenity;
+        }
+
+        private bool AmenityExists(int id)
+        {
+            return _context.Amenities.Any(e => e.Id == id);
+        }
+    }
+}
